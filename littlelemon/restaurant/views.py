4c91c17aa22c55df3f080b2065ebaabb41b23f94
Mitchell Gruner <mit.gruner@gmail.com>
2024-03-09 11:38:07 -0600

    add class 'BookingViewSet' to Booking model

--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from rest_framework import generics
-from .serializers import MenuSerializer
-from .models import Menu
+from rest_framework import generics, viewsets
+from .serializers import BookingSerializer, MenuSerializer
+from .models import Booking, Menu
 
 def index(request):
     return render(request, 'index.html', {})
+
+class BookingViewSet (viewsets.ViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
 
 class MenuItemsView (generics.ListCreateAPIView):
     queryset = Menu.objects.all()
